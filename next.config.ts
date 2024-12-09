4c5649c5967122c46759e107c838035d13468afe
Mehdi AKhdar Idrissi <idrmdi@gmail.com>
2024-12-09 16:01:59 +0100

    firts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+console.log(process.env.DATABASE_URL);
 
 export default nextConfig;
